2750fab183814c27d78eaa92d6cb448780c2a2bd
Sunny sheth <ssheth@aecordigital.com>
2021-05-23 01:03:00 +0530

    Complete the getFutureEventsWithWorkshops function to only shows events that have not started changes

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Event;
+use Carbon\Carbon;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -176,6 +177,9 @@ class EventsController extends BaseController
      */
 
     public function getFutureEventsWithWorkshops() {
-        throw new \Exception('implement in coding task 2');
+        return Event::with('workshops')
+                        ->whereHas('workshops', function($q){
+                            $q->where('start', '>=', Carbon::now()->format('Y-m-d h:i:s'));
+                        })->get()->toArray();
     }
 }
